71c31bc00fa4b9a788a7ac4db7fe2c63f2a53810
helei1024 <kkwp@163.com>
2021-02-14 14:15:31 +0800

    提交测试

---
+++ src/Test.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class Test {
+	public static void main(String[] args) {
+		int[] arr = new int[] {3,1,9,7,0,99,19};
+		
+		int[] result = mySortArray(arr);
+		System.out.println(Arrays.toString(result));
+		
+		
+		int[] arr1 = new int[] {87,1,99,19};
+		int[] result1 = mySortArray(arr1);
+		System.out.println(Arrays.toString(result1));
+	}
+	
+	public static int[] mySortArray(int[] array) {
+		for (int i = 1; i < array.length; i++) {
+			int key = array[i];
+			int j = i - 1;
+			while (j >= 0 && array[j] > key) {
+				array[j + 1] = array[j];
+				j--;
+			}
+			array[j + 1] = key;
+		}
+
+		return array;
+	}
+}
